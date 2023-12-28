a8bdb222f61c4f9ba0635947a3da98e4262e545b
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-12-28 11:23:40 -0500

    Include crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
+#![doc = include_str!("../README.md")]
+
+/// The library file of the application.
+/// This file contains all module definitions and crate-level
+/// declarations. Also contains app logic for the entry point
+/// of the application, located in `main.rs`.
+
 mod tokenizer;
+
+
+
+/// Project Unit Tests
+/// A list of tests to run independently of the main program.
+/// This is mainly just to ensure each step of the program
+/// is working as intended during development.
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn it_works() {
+		assert_eq!(2 + 2, 4);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![doc = include_str!("../README.md")]
+
+/// The entry point of the application
+/// This is the code that runs when the compiled binary is executed.
+/// Note that this does *not* contain the application logic and
+/// module definitions, that is done in `lib.rs`.
 fn main() {
     println!("Hello, world!");
 }
