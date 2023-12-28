6c05e9f8ad5691ed2deade26aefa626efdf6efb8
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2023-12-28 11:42:23 -0500

    Add tokenizer handler for custom function `slog`

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,59 +1,77 @@
-use logos::Logos;
+use logos::{Lexer, Logos};
 
 /// A list of C tokens that are used in the parser.
 #[derive(Debug, Clone, PartialEq, Logos)]
 #[logos(skip r"[ \t\n\f]+")]
-enum Token {
-	#[token("int")]
-	Int,
-	#[token("char")]
-	Char,
-	#[token("long")]
-	Long,
-	#[token("short")]
-	Short,
-	#[token("float")]
-	Float,
-	#[token("double")]
-	Double,
-	#[token("void")]
-	Void,
-	#[token("struct")]
-	Struct,
-	#[token("union")]
-	Union,
-	#[token("enum")]
-	Enum,
-	#[token("signed")]
-	Signed,
-	#[token("unsigned")]
-	Unsigned,
-	#[token("const")]
-	Const,
-	#[token("break")]
-	Break,
-	#[token("continue")]
-	Continue,
-	#[token("else")]
-	Else,
-	#[token("for")]
-	For,
-	#[token("do")]
-	Do,
-	#[token("while")]
-	While,
-	#[token("if")]
-	If,
-	#[token("return")]
-	Return,
-	// The following keywords are compiler
-	// suggestions that don't really mean anything
-	// in the context of the parser. Therefore they are
-	// ignored.
-	// #[token("volatile")]
-	// Volatile,
-	// #[token("auto")]
-	// Auto,
-	// #[token("register")]
-	// Register,
+pub enum Token<'a> {
+    #[token("int")]
+    Int,
+    #[token("char")]
+    Char,
+    #[token("long")]
+    Long,
+    #[token("short")]
+    Short,
+    #[token("float")]
+    Float,
+    #[token("double")]
+    Double,
+    #[token("void")]
+    Void,
+    #[token("struct")]
+    Struct,
+    #[token("union")]
+    Union,
+    #[token("enum")]
+    Enum,
+    #[token("signed")]
+    Signed,
+    #[token("unsigned")]
+    Unsigned,
+    #[token("const")]
+    Const,
+    #[token("break")]
+    Break,
+    #[token("continue")]
+    Continue,
+    #[token("else")]
+    Else,
+    #[token("for")]
+    For,
+    #[token("do")]
+    Do,
+    #[token("while")]
+    While,
+    #[token("if")]
+    If,
+    #[token("return")]
+    Return,
+    #[token("slog")]
+    Slog(&'a str),
+}
+
+// Handlers
+/// Handler to get inner string of log function
+fn slog<'a>(lex: &'a mut Lexer<Token>) -> Option<&'a str> {
+    let slice = lex.slice();
+    let mut left: usize = 0;
+    let mut right: usize = slice.len() - 1;
+    let mut db: bool = false;
+    let (mut db1, mut db2) = (false, false);
+    for (i, c) in slice.chars().enumerate() {
+        if c == r#"""# {
+            if !db {
+                left = i;
+            } else {
+                db = true;
+                right = i;
+            }
+            if db1 == false {
+                db1 == true;
+            } else {
+                db2 == true
+            }
+        }
+    }
+    db2.then(|| db1.then(|| &slice[left..=right]).unwrap())
 }
