bb4001d03aa72821af5123c4677189579ad7e86f
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2024-01-08 19:56:31 -0500

    Add C struct representation and add derive macros to all IR structs/enums

--- src/ir/token_repr.rs
+++ src/ir/token_repr.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+
+/// Type to represent all basic types of C.
+/// This enum isn't meant to be exhaustive, rather
+/// only cover the define-able types in C that make up other C
+/// concepts, such as structs or functions.
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum CType {
     Int,
     Short,
@@ -12,4 +19,13 @@ enum CType {
     Void,
     Ptr(CType),
     Array(CType),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct CStructField(CType, String);
+
+#[derive(Debug, Clone, PartialEq)]
+struct CStruct {
+    name: String,
+    field: Vec<CStructField>,
 }
